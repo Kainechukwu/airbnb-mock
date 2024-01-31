f03b7bf4c2e31f5423a845266fad88afef5a9ff2
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-01-31 15:02:33 +0100

    updated UI for curency

--- src/components/ui/community/CommunityContentBody.tsx
+++ src/components/ui/community/CommunityContentBody.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import CommunityBodyOption from './CommunityBodyOption'
 import { languageOptions, currencyOptions } from '../../../utils'
+import Switch from '../Switch'
+
 export default function CommunityContentBody({
   selected
 }: {
@@ -24,10 +26,30 @@ export default function CommunityContentBody({
   }, [selected, setHeadingText, setOptions])
   return (
     <div className="mt-10 flex flex-col items-start justify-start pb-4">
+      {selected?.toLowerCase() !== 'currency' && (
+        <div className="mb-10 flex cursor-pointer items-center gap-2 rounded-[12px] bg-[#F7F7F7]  p-4 ">
+          <div className="mr-2 flex flex-col items-start">
+            <div className="flex gap-2 items-center mb-1">
+              <span className="text-base text-[#222222]">Translation</span>
+              <img
+                className="size-[24px]"
+                src="/currencycommunityimg.png"
+                alt=""
+              />
+            </div>
+            <span className="text-sm text-[#7F7F7F]">
+              Automatically translate descriptions and reviews to English
+            </span>
+          </div>
+
+          <div>
+            <Switch />
+          </div>
+        </div>
+      )}
       <span className="mb-4 text-[20px] font-medium text-[#222222]">
         {headingText}
       </span>
-
       <div className="grid w-full grid-cols-5 gap-x-6 gap-y-4">
         {options?.map((option) => (
           <div key={option.id} onClick={() => setSelectedOption(option)}>
