a9fa5b03f812c7ea83729449159a17459babeb01
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-02-01 14:09:39 +0100

    updated intersection observer

--- src/components/ui/searchComponent/SearchComponent.tsx
+++ src/components/ui/searchComponent/SearchComponent.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable tailwindcss/no-custom-classname */
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react'
+import { Transition } from '@headlessui/react'
+
 import { HiOutlineSearch } from 'react-icons/hi'
 import Where from './Where'
 import Check from './Check'
@@ -40,41 +42,76 @@ export default function SearchComponent() {
         <div
           className={`relative top-[-11px] ${
             !isShowingBigSearch ? 'flex' : 'hidden'
-          }`}
+          }   
+          transition-opacity duration-[5000ms] ease-in-out ${
+            !isShowingBigSearch ? 'opacity-100' : 'opacity-0'
+          } `}
         >
-          <SmallSearchComponent />
+          <Transition.Root show={!isShowingBigSearch} as={Fragment}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div>
+                <SmallSearchComponent />{' '}
+              </div>
+            </Transition.Child>
+          </Transition.Root>
         </div>
       </div>
-      <div
-        className={`${
-          isShowingBigSearch ? 'flex' : 'hidden'
-        } searchBoxShadow relative mx-auto mb-6  w-full max-w-[850px] items-center  gap-0 rounded-[40px] border border-gray-200 bg-white`}
-      >
-        <div
-          onClick={() => handleSearch()}
-          className="absolute  right-2 z-[2] flex size-[48px] items-center justify-center rounded-full bg-[#EC4C60] text-[18px] text-white"
+
+      <Transition.Root show={isShowingBigSearch} as={Fragment}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-500"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <HiOutlineSearch />
-        </div>
-        {searchFilters?.map((item, index) => (
-          <div className="flex items-center" key={item.title}>
+          <div>
             <div
-              className={`${
-                index == 0 ? 'hidden' : ''
-              } h-[26px] w-px bg-gray-200`}
-            ></div>
-            <SearchButtonOption item={item}>
-              <div>
-                {item.title.toLowerCase().includes('where') && <Where />}
+              className={`${isShowingBigSearch ? 'flex' : 'hidden'}
+        transition-opacity duration-500 ease-in-out ${
+          isShowingBigSearch ? 'opacity-100' : 'opacity-0'
+        } searchBoxShadow relative mx-auto mb-6  w-full max-w-[850px] items-center  gap-0 rounded-[40px] border border-gray-200 bg-white`}
+            >
+              <div
+                onClick={() => handleSearch()}
+                className="absolute  right-2 z-[2] flex size-[48px] items-center justify-center rounded-full bg-[#EC4C60] text-[18px] text-white"
+              >
+                <HiOutlineSearch />
               </div>
-              <div>
-                {item.title.toLowerCase().includes('check') && <Check />}
-              </div>
-              <div>{item.title.toLowerCase().includes('who') && <Who />}</div>
-            </SearchButtonOption>
+              {searchFilters?.map((item, index) => (
+                <div className="flex items-center" key={item.title}>
+                  <div
+                    className={`${
+                      index == 0 ? 'hidden' : ''
+                    } h-[26px] w-px bg-gray-200`}
+                  ></div>
+                  <SearchButtonOption item={item}>
+                    <div>
+                      {item.title.toLowerCase().includes('where') && <Where />}
+                    </div>
+                    <div>
+                      {item.title.toLowerCase().includes('check') && <Check />}
+                    </div>
+                    <div>
+                      {item.title.toLowerCase().includes('who') && <Who />}
+                    </div>
+                  </SearchButtonOption>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+        </Transition.Child>
+      </Transition.Root>
     </div>
   )
 }
