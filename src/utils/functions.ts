17e803bc0e5dd067b99cae968ec1253231f9f411
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-02-02 02:20:09 +0100

    updated loc file

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -10,6 +10,7 @@ interface TravelParams {
 
 export function constructUrlParams(obj: TravelParams): string {
   const urlParams = Object.entries(obj)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     .filter(([key, value]) => value > 0 || String(value).length > 0)
     .map(([key, value]) => `${key}=${value}`)
     .join('&')
